f096c9540ceafe0d01dd3e50e9a766f2eb876da7
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-10 16:18:43 +0200

    reformat_code_model_validator

--- model_validator.py
+++ model_validator.py
@@ -1,14 +1,19 @@
 import validation_utils as vu
+import sys
 
 
+def validate(path: str) -> None:
 
-def validate():
+    folder_class1 = 'C:\\Users\\Alternanza\\Downloads\\moto'
+    class1 = "moto"
+    folder_class2 = 'C:\\Users\\Alternanza\\Downloads\\bici'
+    class2 = "bici"
 
-    correct_predition, tot_image = vu.test_all_image()
-    accuracy =  correct_predition / tot_image
-    with open('C:\\Users\\Alternanza\\Downloads\\accuracy.txt','w') as file:
-        file.write(f"Accuracy: {accuracy*100}%")
+    correct_predition, tot_image = vu.test_all_image(folder_class1, folder_class2, class1, class2)
+    accuracy = correct_predition / tot_image
+    with open(path, 'w') as file:
+        file.write(f"Accuracy: {accuracy * 100}%")
 
 
 if __name__ == "__main__":
-    validate()
+    validate(sys.argv[0])
