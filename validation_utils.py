2eb4336cd0c68b5716c628f296174d19a8487c2e
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-10 16:19:11 +0200

    reformat_code_valid_ut.py

--- validation_utils.py
+++ validation_utils.py
@@ -1,21 +1,15 @@
-
 from predictor import test_image
 
 from io_utils import load_all_images
 
-def test_all_image():
-    folder_class1 = 'C:\\Users\\Alternanza\\Downloads\\moto'
-    class1 = "moto"
-    folder_class2 = 'C:\\Users\\Alternanza\\Downloads\\bici'
-    class2 = "bici"
+
+def test_all_image(folder_class1: str, folder_class2: str, class1: str, class2: str) -> (int, int):
+
     image = load_all_images(folder_class1, class1) + load_all_images(folder_class2, class2)
 
     num_correct_predictions = 0
     for image_path, true_label in image:
-        predicted_class , confidence_score = test_image(image_path,true_label)
-        if(predicted_class == true_label ):
+        predicted_class, confidence_score = test_image(image_path, true_label)
+        if predicted_class == true_label:
             num_correct_predictions += 1
-    return num_correct_predictions,len(image)
-
-
-
+    return num_correct_predictions, len(image)
