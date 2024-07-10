b6267f3900a8bf433f46af108cf7e2d62dcc8932
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-10 16:18:08 +0200

    reformat_code_img_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,17 +1,20 @@
 import cv2
 import numpy as np
 
-def preprocces_image(img,altezza,larghezza):
-    image_resized=resize(img,altezza,larghezza)
+
+def preprocces_image(img: np.array, altezza: int, larghezza: int) -> np.array:
+    image_resized = resize(img,altezza,larghezza)
     image_normalized = (image_resized / 127.5) - 1
     image_preprocessed = to_tensor(image_normalized)
     return image_preprocessed
 
 
-def resize(img,altezza,larghezza):
+def resize(img: np.array, altezza: int, larghezza: int) -> np.array:
     image_resized = cv2.resize(img, (larghezza, altezza))
     return image_resized
-def to_tensor(img):
+
+
+def to_tensor(img: np.array) -> np.array:
     num_images = 1
     altezza = img.shape[0]
     larghezza = img.shape[1]
