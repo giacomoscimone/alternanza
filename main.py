c92139bd70444cde76cf0cc8e4d59c538699114c
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-10 16:18:30 +0200

    reformat_code_main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import predictor as pr
-import io_utils as io
 import sys
 
+
 def main():
-    image = 'C:\\Users\\Alternanza\\Downloads\\bici3.jpg'
+    image = sys.argv[1]
+    # 'C:\\Users\\Alternanza\\Downloads\\bici3.jpg'
 
     predicted_class, prediction_confidence = pr.test_image(image, "bici")
     print("classe: ", predicted_class, f"confidence:{prediction_confidence * 100} %")
