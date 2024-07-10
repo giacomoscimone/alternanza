19002d0702ec67f09bb180181488d6fd56fa3dca
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-10 16:18:21 +0200

    reformat_code_io_ut.py

--- io_utils.py
+++ io_utils.py
@@ -1,23 +1,26 @@
 import os
 import cv2
+import numpy as np
 
 
-def load_image(path):
+def load_image(path: str) -> np.array:
     image = cv2.imread(path)
     return image
 
-def show_image(img):
-    cv2.imshow('Original Image', img)
+
+def show_image(img: np.array, img_title: str) -> None:
+    cv2.imshow(img_title, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def save_image(img,path):
-    return cv2.imwrite(path,img)
+
+def save_image(img: np.array, path: str) -> (str, np.array):
+    return cv2.imwrite(path, img)
 
 
-def load_all_images(path,true_label):
+def load_all_images(path: str, true_label: str) -> np.array:
     image_paths = []
     for filename in os.listdir(path):
         full_path = os.path.join(path, filename)
-        image_paths.append((full_path, tag))
+        image_paths.append((full_path, true_label))
     return image_paths
