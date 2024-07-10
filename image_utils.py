ff42d97b4fb0ee7764475ce596c87cc6b925f9c2
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-10 15:25:39 +0200

    module_img_utils

---
+++ image_utils.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+def preprocces_image(img,altezza,larghezza):
+    image_resized=resize(img,altezza,larghezza)
+    image_normalized = (image_resized / 127.5) - 1
+    image_preprocessed = to_tensor(image_normalized)
+    return image_preprocessed
+
+
+def resize(img,altezza,larghezza):
+    image_resized = cv2.resize(img, (larghezza, altezza))
+    return image_resized
+def to_tensor(img):
+    num_images = 1
+    altezza = img.shape[0]
+    larghezza = img.shape[1]
+    num_color_channels = img.shape[2]
+    tensore = (np.asarray(img, dtype=np.float32).reshape(num_images,larghezza,altezza,num_color_channels))
+    return tensore
