05cf2f9d7803cef2e3b5e5e00e5d2dd5b25ecb17
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-15 14:31:10 +0200

    reformat code predictor

--- predictor.py
+++ predictor.py
@@ -1,28 +1,15 @@
 from typing import Any
 
 import numpy as np
-from keras.models import load_model as load_tf_model
-from image_utils import preprocces_image
-import cv2
+from image_utils import preproces_image
+
+classes = ["bici", "moto"]
 
 
-def load_model(path: Any) -> str:
-    model = load_tf_model(path, compile=False)
-    return model
-
-
-def predict(img: np.array) -> (str, float):
-    modello = load_model('C:\\Users\\Alternanza\\Downloads\\keras_model.h5')
-    prediction = modello.predict(img)
+def predict(img: np.array, model: Any) -> tuple[str, float]:
+    prediction = model.predict(img)
     index = np.argmax(prediction)
-    class_names = ["bici", "moto"]
-    predicted_class = class_names[index]
+    predicted_class = classes[index]
     prediction_confidence = prediction[0][index]
     return predicted_class, prediction_confidence
 
-
-def test_image(img: np.array, true_label: str) -> (str, int):
-    img = cv2.imread(img)
-    img_preprocessed = preprocces_image(img, 224, 224)
-    predicted_class, confidence_score = predict(img_preprocessed)
-    return predicted_class, confidence_score
