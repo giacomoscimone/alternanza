fb193ad26eb66e9a7ec7258de99a632bf276e919
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-15 17:39:05 +0200

    logger predictor

--- predictor.py
+++ predictor.py
@@ -1,13 +1,13 @@
 from typing import Any
-
 import numpy as np
-from image_utils import preproces_image
+import logging
 
 classes = ["bici", "moto"]
 
 
 def predict(img: np.array, model: Any) -> tuple[str, float]:
     prediction = model.predict(img)
+    logging.debug("predizione completata")
     index = np.argmax(prediction)
     predicted_class = classes[index]
     prediction_confidence = prediction[0][index]
